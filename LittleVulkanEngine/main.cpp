5852da66c16787fb1d02c782a315244b41a70510
Colin Leary <colinleary123@gmail.com>
2024-05-08 13:05:28 -0400

    Initial Commit

---
+++ LittleVulkanEngine/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  LittleVulkanEngine
+//
+//  Created by Colin Leary on 5/8/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
