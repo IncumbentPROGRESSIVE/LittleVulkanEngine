8e15a2692630a0fdde7eedc303d590abdc08097c
Colin Leary <colinleary123@gmail.com>
2025-03-16 09:02:31 -0400

    ktx

--- main.cpp
+++ main.cpp
@@ -261,7 +261,7 @@ class HelloTriangleApplication {
     void loadTexture() {
         ktxTexture* texture;
         KTX_error_code result = ktxTexture_CreateFromNamedFile(
-            "RoomONE.ktx2",  // Ensure this is the correct path
+            "/Users/colinleary/Downloads/RoomONE.ktx2",  // Ensure this is the correct path
             KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT,
             &texture
         );
@@ -1313,8 +1313,13 @@ void HelloTriangleApplication::createImage(uint32_t width, uint32_t height, VkFo
     vkBindImageMemory(device, image, imageMemory, 0);
 }
 
-void HelloTriangleApplication::transitionImageLayout(VkImage image, VkFormat format,
-                                                     VkImageLayout oldLayout, VkImageLayout newLayout) {
+void HelloTriangleApplication::transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout) {
+    std::cout << "🔄 Transitioning image layout from " << oldLayout << " to " << newLayout << std::endl;
+
+    if (oldLayout == newLayout) {
+        throw std::runtime_error("❌ ERROR: Attempting to transition image to the same layout!");
+    }
+
     VkCommandBuffer commandBuffer = beginSingleTimeCommands();
 
     VkImageMemoryBarrier barrier{};
@@ -1338,12 +1343,18 @@ void HelloTriangleApplication::transitionImageLayout(VkImage image, VkFormat for
         barrier.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
         sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
         destinationStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
-    } else {
-        throw std::invalid_argument("Unsupported layout transition!");
+    }
+    else if (oldLayout == VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL && newLayout == VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL) {
+        barrier.srcAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
+        barrier.dstAccessMask = VK_ACCESS_SHADER_READ_BIT;
+        sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
+        destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+    }
+    else {
+        throw std::runtime_error("❌ ERROR: Unsupported layout transition!");
     }
 
-    vkCmdPipelineBarrier(commandBuffer, sourceStage, destinationStage,
-                         0, 0, nullptr, 0, nullptr, 1, &barrier);
+    vkCmdPipelineBarrier(commandBuffer, sourceStage, destinationStage, 0, 0, nullptr, 0, nullptr, 1, &barrier);
 
     endSingleTimeCommands(commandBuffer);
 }
@@ -1419,5 +1430,3 @@ void HelloTriangleApplication::endSingleTimeCommands(VkCommandBuffer commandBuff
 
     vkFreeCommandBuffers(device, commandPool, 1, &commandBuffer);
 }
-
-
